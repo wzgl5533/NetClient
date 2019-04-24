4a782a5fb1d3e85e16cb273e974eefc93f710aa6
quanlianghu <708937822@qq.com>
2019-04-24 16:44:04 +0800

    优化log开启

--- netclient/src/main/java/com/qlh/netclient/utils/Logs.java
+++ netclient/src/main/java/com/qlh/netclient/utils/Logs.java
@@ -1,7 +1,7 @@
 package com.qlh.netclient.utils;
 
 import android.util.Log;
-import com.qlh.netclient.BuildConfig;
+import com.qlh.netclient.interceptor.LoggingInterceptor;
 
 /**
  * Created by QLH on 2017/8/29.
@@ -12,67 +12,67 @@ import com.qlh.netclient.BuildConfig;
 public class Logs {
 
     public static void v(String tag, String msg) {
-        if (BuildConfig.DEBUG) {
+        if (LoggingInterceptor.isDebug) {
             Log.v(tag, msg);
         }
     }
 
     public static void v(String tag, String msg, Throwable tr) {
-        if (BuildConfig.DEBUG) {
+        if (LoggingInterceptor.isDebug) {
             Log.v(tag, msg, tr);
         }
     }
 
     public static void d(String tag, String msg) {
-        if (BuildConfig.DEBUG) {
+        if (LoggingInterceptor.isDebug) {
             Log.d(tag, msg);
         }
     }
 
     public static void d(String tag, String msg, Throwable tr) {
-        if (BuildConfig.DEBUG) {
+        if (LoggingInterceptor.isDebug) {
             Log.d(tag, msg, tr);
         }
     }
 
     public static void i(String tag, String msg) {
-        if (BuildConfig.DEBUG) {
+        if (LoggingInterceptor.isDebug) {
             Log.i(tag, msg);
         }
     }
 
     public static void i(String tag, String msg, Throwable tr) {
-        if (BuildConfig.DEBUG) {
+        if (LoggingInterceptor.isDebug) {
             Log.i(tag, msg, tr);
         }
     }
 
     public static void w(String tag, String msg) {
-        if (BuildConfig.DEBUG) {
+        if (LoggingInterceptor.isDebug) {
             Log.w(tag, msg);
         }
     }
 
     public static void w(String tag, String msg, Throwable tr) {
-        if (BuildConfig.DEBUG) {
+        if (LoggingInterceptor.isDebug) {
             Log.w(tag, msg, tr);
         }
     }
 
     public static void w(String tag, Throwable tr) {
-        if (BuildConfig.DEBUG) {
+        if (LoggingInterceptor.isDebug) {
             Log.w(tag, tr);
         }
     }
 
     public static void e(String tag, String msg) {
-        if (BuildConfig.DEBUG) {
+        if (LoggingInterceptor.isDebug) {
             Log.e(tag, msg);
         }
     }
 
     public static void e(String tag, String msg, Throwable tr) {
-        if (BuildConfig.DEBUG) {
+        if (LoggingInterceptor.isDebug) {
             Log.e(tag, msg, tr);
         }
     }
